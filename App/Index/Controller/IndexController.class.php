461649de9068e91c51c8a249a37d601e4af7b921
wangjinhua520 <997885637@qq.com>
2016-05-26 15:09:11 +0800

    第一次提交代码
    新建项目，首次把代码加进去

---
+++ App/Index/Controller/IndexController.class.php
@@ -0,0 +1,19 @@
+<?php
+namespace Index\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+    	$this->display();
+    }
+
+    public function left(){
+    	$this->display();
+    }
+
+    public function right(){
+    	$this->display();
+    }
+    public function top(){
+    	$this->display();
+    }
+}
